f7400d7e37abf949d60fc2aa63f570c74a63dc3a
Paola_C <paola.cyprien@hetic.net>
2023-10-09 20:06:49 +0200

    header -Ok

--- back-office/src/components/organims/Header/Header.tsx
+++ back-office/src/components/organims/Header/Header.tsx
@@ -1,12 +1,60 @@
-import React, { FC } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 interface HeaderProps {}
 
-const Header: FC<HeaderProps> = () => (
+const Header: FC<HeaderProps> = () => {
+
+  // const {isAuthenticated,handleLogout,userRole ,username} = useContext(AuthContext);
+const [ subnavbarIsOpen, setSubnavbarIsOpen ] = useState(true);
+const [ username, setUsername ] = useState('Paola CYPRIEN');
+const [ isAuthenticated, setIsAuthenticated ] = useState(true);
+
+
+  const getFirstLetters = (value :any) => {
+    let username = value;
+    let usernameStrimed = username.trim();
+    username = usernameStrimed.split(' ');
+    let firstname = Array.from(username[0])[0];
+    let lastname = Array.from(username[username.length - 1])[0];
+    return firstname + '' + lastname ;
+  }
+  return (
   <div className="Header" data-testid="Header">
-    Header Component
+    <button onClick={() => setSubnavbarIsOpen(!subnavbarIsOpen)} tabIndex={0}>
+      <Link to="#" className='gestion' >
+        <FontAwesomeIcon icon={faUser} /> 
+        <span id="username" className='desktop'> {username && username !== undefined ? username : ''  }    </span>   
+        <span id="username" className='smartphone'> {username && username !== undefined ? getFirstLetters(username) : ''  }    </span>
+        <span className={`chevron ${ subnavbarIsOpen ? 'open' : ''}`} ></span>
+      </Link>
+    </button>
+
+    <ul className={`sub-menu ${ subnavbarIsOpen ? 'open' : ''}`}  tabIndex={0} >
+              { isAuthenticated && (    
+                <li >
+                  <Link to="/espace-personnel"> Espace personnel  </Link> 
+                </li>
+              )}
+              {( isAuthenticated !== false && isAuthenticated !== null) && (
+              <li>
+                <Link to="/messagerie">Messagerie</Link>
+              </li>
+              )}
+
+              {(isAuthenticated !== false && isAuthenticated !== null) && (
+              <li>
+                <Link to="/" >
+                Se déconnecter
+                </Link>
+              </li> )}
+
+              
+          </ul>
   </div>
-);
+)};
 
 export default Header;
--- back-office/src/layout/Layout.tsx
+++ back-office/src/layout/Layout.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import Sidebar from '../components/organims/Sidebar/Sidebar';
 import "./Layout.scss";
 import RoutesNavigation from '../routes/RoutesNavigation';
+import Header from '../components/organims/Header/Header';
 
 const Layout = () => {
   return (
     <div className="global-container">
         <Sidebar />
-        <main>
-           <RoutesNavigation />
-        </main>
+        <div className='main-content'>
+            <nav>
+              <Header />
+            </nav>
+            <main>
+              <RoutesNavigation />
+            </main>
+        </div>
     </div>
   )
 }
